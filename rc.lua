8eb507409b1d5996002b4c42c1969fe631bef5f3
Smugd <smugdoggie@gmail.com>
2017-06-24 13:52:17 +0900

    Implement floating windows

--- rc.lua
+++ rc.lua
@@ -6,14 +6,42 @@ local awful = require("awful")
 
 -- Create tag table for each screen
 awful.screen.connect_for_each_screen(function(s)
-    awful.tag({ "1" }, s, awful.layout.suit.spiral)
+    awful.tag({ "1" }, s, awful.layout.suit.floating)
 end)
 
 -- Global key bindings
-root.keys(gears.table.join(
+globalkeys = gears.table.join(
+    -- Awesome
     awful.key({ "Mod4", "Control" }, "q", awesome.quit),
     awful.key({ "Mod4", "Control" }, "r", awesome.restart),
-    awful.key({ "Mod4" }, "Return", function() awful.spawn("xterm") end)))
+    -- Standard applications
+    awful.key({ "Mod4" }, "Return", function() awful.spawn("xterm") end),
+    -- Focus
+    awful.key({ "Mod4" }, "Tab", function()
+        awful.client.focus.history.previous()
+        if client.focus then
+            client.focus:raise()
+        end
+    end))
+
+root.keys(globalkeys)
+
+clientkeys = gears.table.join(
+    awful.key({ "Mod4" }, "w", function(c) c:kill() end))
+
+clientbuttons = gears.table.join(
+    awful.button({ }, 1, function(c) client.focus = c; c:raise() end),
+    awful.button({ "Mod4" }, 1, awful.mouse.client.move),
+    awful.button({ "Mod4" }, 3, awful.mouse.client.resize))
+
+-- Setup rules for appearing windows
+awful.rules.rules = {
+    { rule = { },
+      properties = {
+        focus = awful.client.focus.filter,
+        keys = clientkeys,
+        buttons = clientbuttons } }
+}
 
 -- Make focus follow mouse
 client.connect_signal("mouse::enter", function(c) client.focus = c end)
