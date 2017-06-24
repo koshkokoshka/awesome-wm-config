625ec78ded2f2520fac49f28059721799c1e2cd3
Smugd <smugdoggie@gmail.com>
2017-06-24 09:54:36 +0900

    Comment code

--- rc.lua
+++ rc.lua
@@ -1,15 +1,19 @@
 -- vim: tabstop=4 shiftwidth=4 expandtab
 
+-- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
 
+-- Create tag table for each screen
 awful.screen.connect_for_each_screen(function(s)
     awful.tag({ "1" }, s, awful.layout.suit.spiral)
 end)
 
+-- Global key bindings
 root.keys(gears.table.join(
     awful.key({ "Mod4", "Control" }, "q", awesome.quit),
     awful.key({ "Mod4", "Control" }, "r", awesome.restart),
     awful.key({ "Mod4" }, "Return", function() awful.spawn("xterm") end)))
 
+-- Make focus follow mouse
 client.connect_signal("mouse::enter", function(c) client.focus = c end)
