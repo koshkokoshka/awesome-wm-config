17eeedf2ee5af35bb01c9b49e05423a63c1237a7
smugd <smugdoggie@gmail.com>
2017-06-25 16:38:03 +0900

    Implement basic wibar functionality

--- rc.lua
+++ rc.lua
@@ -3,10 +3,36 @@
 -- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
+-- Widget and layout library
+local wibox = require("wibox")
 
 -- Create tag table for each screen
+local tasklistbuttons = gears.table.join(
+    -- LMB
+    -- Focus on a client
+    awful.button({ }, 1, function(c)
+        if c == client.focus then
+            c.minimized = true
+        else
+            c.minimized = false
+            c:raise()
+            client.focus = c;
+        end
+    end),
+    -- MMB
+    -- Close a client
+    awful.button({ }, 2, function(c) c:kill() end)
+)
+
 awful.screen.connect_for_each_screen(function(s)
     awful.tag({ "1" }, s, awful.layout.suit.spiral)
+
+    s.tasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, tasklistbuttons)
+    s.wibar = awful.wibar({ position = "top", screen = s })
+    s.wibar:setup {
+        layout = wibox.layout.align.horizontal,
+        s.tasklist
+    }
 end)
 
 -- Global key bindings
