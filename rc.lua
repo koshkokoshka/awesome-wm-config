53b36db761b02cb2ff9ec33dfd561510c195d4fc
smugd <smugdoggie@gmail.com>
2017-07-08 13:17:24 +0900

    Add tag manipulation hotkeys

--- rc.lua
+++ rc.lua
@@ -38,6 +38,31 @@ globalkeys = gears.table.join(
         end
     end))
 
+-- Tags manipulation keys
+for i = 1, 9 do
+    globalkeys = gears.table.join(globalkeys,
+        -- MOD + [1-9]
+        -- View tag only
+        awful.key({ modkey }, "#" .. i + 9, function()
+            local tag = awful.screen.focused().tags[i]
+            if tag then
+                tag:view_only()
+            end
+        end),
+        -- MOD + SHIFT + [1-9]
+        -- Move focused client to tag
+        awful.key({ modkey, "Shift" }, "#" .. i + 9, function()
+            if client.focus then
+                local tag = client.focus.screen.tags[i]
+                if tag then
+                    client.focus:move_to_tag(tag)
+                end
+            end
+        end)
+    )
+end
+
+
 -- Client keys
 clientkeys = gears.table.join(
     -- CTRL + W
